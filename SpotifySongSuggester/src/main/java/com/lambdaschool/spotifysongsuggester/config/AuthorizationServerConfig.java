d4dc301a57a9bec20563feecd8ccd6adf507892b
Nazifa Hossain <44625143+ampers-and@users.noreply.github.com>
2019-10-28 14:38:56 -0700

    Update AuthorizationServerConfig.java

--- SpotifySongSuggester/src/main/java/com/lambdaschool/spotifysongsuggester/config/AuthorizationServerConfig.java
+++ SpotifySongSuggester/src/main/java/com/lambdaschool/spotifysongsuggester/config/AuthorizationServerConfig.java
@@ -14,8 +14,6 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter
 {
-//    static final String CLIENT_ID = System.getenv("OAUTHCLIENTID"); // read from environment variable
-//    static final String CLIENT_SECRET = System.getenv("OAUTHCLIENTSECRET"); // read from environment variable
      static final String CLIENT_ID = "lambda-client";
      static final String CLIENT_SECRET = "lambda-secret";
 
